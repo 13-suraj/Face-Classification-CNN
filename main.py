c0a7e545279a364243aecda61b5ffcb226620656
13-suraj <surajkeshari260@gmail.com>
2025-10-12 21:25:11 +0530

    Prepared Base Model

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from src.FaceClassification import logger
 from src.FaceClassification.pipeline.stage_01_data_ingestion import DataIngestionConfigPipeline
+from src.FaceClassification.pipeline.stage_02_prepare_base_model import PrepareBaseModelPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
-
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataIngestionConfigPipeline()
@@ -11,3 +11,13 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+STAGE_NAME = "Prepare Base Model Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = PrepareBaseModelPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx=========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
--- src/FaceClassification/entity/config_entity.py
+++ src/FaceClassification/entity/config_entity.py
@@ -1,4 +1,3 @@
-import os
 from dataclasses import dataclass
 from pathlib import Path
 
@@ -8,3 +7,14 @@ class DataIngestionConfig:
     source_url: str
     local_data_file: Path
     unzip_dir: Path
+
+@dataclass(frozen = True)
+class PrepareBaseModelConfig:
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model_path: Path
+    params_image_size: list
+    params_include_top: bool
+    params_classes: int
+    params_weights: str
+    params_learning_rate: float
