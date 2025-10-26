edf6e8002dd165300168bfc7882d17bb1d647fbf
13-suraj <surajkeshari260@gmail.com>
2025-10-26 21:59:11 +0530

    created flask web app

--- src/FaceClassification/pipeline/predict.py
+++ src/FaceClassification/pipeline/predict.py
@@ -1,7 +1,7 @@
 import numpy as np
+import os
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
-import os
 
 class PredictionPipeline:
     def __init__(self, filename):
@@ -9,16 +9,17 @@ class PredictionPipeline:
 
     def predict(self):
         model = load_model(os.path.join("artifacts", "training", "model.keras"))
-        imagename = self.filename
-        test_image = image.load_image(imagename, target_size = (224, 244))
+
+        test_image = image.load_img(self.filename, target_size=(224, 224))
         test_image = image.img_to_array(test_image)
-        test_image = np.expand_dims(test_image, axis = 0)
-        result = np.argmax(model.predict(test_image), axis = 1)
-        print(result)
+        test_image = np.expand_dims(test_image, axis=0) / 255.0  # normalize like training
+
+        result = np.argmax(model.predict(test_image), axis=1)
+        print("Raw prediction:", result)
 
         if result[0] == 1:
-            prediction = "Healthy"
-            return [{"image" : prediction}]
+            prediction = "Subhash"
         else:
-            prediction = "Coccidiosis"
-            return [{"image" : prediction}]    
+            prediction = "Suraj"
+
+        return [{"image": prediction}]
